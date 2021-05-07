223aa952b8276ef983c9fc555e0f1aaa1b4c4da3
Angel Rodriguez <54855950+Angel-ui@users.noreply.github.com>
2021-05-06 23:45:31 -0500

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,108 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+    path: '/page',
+      component: () => import('@/views/pages/Index'),
+      children: [
+        {
+          name: 'Lock',
+          path: 'lock',
+          component: () => import('@/views/pages/Lock'),
+        },
+        {
+          name: 'Login',
+          path: 'login',
+          component: () => import('@/views/pages/Login'),
+        },
+        {
+          name: 'Pricing',
+          path: 'pricing',
+          component: () => import('@/views/pages/Pricing'),
+        },
+        {
+          name: 'Register',
+          path: 'register',
+          component: () => import('@/views/pages/Register'),
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: () => import('@/views/dashboard/Index'),
+      children: [
+        // Dashboard
+        {
+          name: 'Dashboard',
+          path: '',
+          component: () => import('@/views/dashboard/Dashboard'),
+        },
+        // Pages
+        {
+          name: 'Teachers',
+          path: 'tables/teachers',
+          component: () => import('@/views/dashboard/tables/Teachers'),
+        },
+        {
+          name: 'Courses',
+          path: 'tables/courses',
+          component: () => import('@/views/dashboard/tables/Courses'),
+        },
+        {
+          name: 'Categories',
+          path: 'tables/categories',
+          component: () => import('@/views/dashboard/tables/Categories'),
+        },
+        {
+          name: 'Users',
+          path: 'tables/users',
+          component: () => import('@/views/dashboard/tables/Users'),
+        },
+        {
+          name: 'User Profile',
+          path: 'pages/user',
+          component: () => import('@/views/dashboard/pages/UserProfile'),
+        },
+        {
+          name: 'Notifications',
+          path: 'components/notifications',
+          component: () => import('@/views/dashboard/component/Notifications'),
+        },
+        {
+          name: 'Icons',
+          path: 'components/icons',
+          component: () => import('@/views/dashboard/component/Icons'),
+        },
+        {
+          name: 'Typography',
+          path: 'components/typography',
+          component: () => import('@/views/dashboard/component/Typography'),
+        },
+        // Tables
+        {
+          name: 'Regular Tables',
+          path: 'tables/regular-tables',
+          component: () => import('@/views/dashboard/tables/RegularTables'),
+        },
+        // Maps
+        {
+          name: 'Google Maps',
+          path: 'maps/google-maps',
+          component: () => import('@/views/dashboard/maps/GoogleMaps'),
+        },
+        // Upgrade
+        {
+          name: 'Upgrade',
+          path: 'upgrade',
+          component: () => import('@/views/dashboard/Upgrade'),
+        },
+      ],
+    },
+  ],
+})
